f569cbffd8fe7acf3cb90d42a3d7ef73a5c3430a
Oksana <familianamy@gmail.com>
2025-07-13 00:06:25 +0300

    update

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,18 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const btn = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+
+btn.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  spanColor.textContent = newColor;
+
+  document.body.style.transition = 'background-color 0.5s ease';
+});
+
+
